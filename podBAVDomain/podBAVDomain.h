7a70e3a1e34a0ad9379e0e1765e2218919f7f2f8
Gmo Ginppian <davidguillermo.lopez.contractor@bbva.com>
2018-07-30 14:41:35 -0500

    Primer commit

---
+++ podBAVDomain/podBAVDomain.h
@@ -0,0 +1,19 @@
+//
+//  podBAVDomain.h
+//  podBAVDomain
+//
+//  Created by Gmo Ginppian on 30/07/18.
+//  Copyright © 2018 BUAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for podBAVDomain.
+FOUNDATION_EXPORT double podBAVDomainVersionNumber;
+
+//! Project version string for podBAVDomain.
+FOUNDATION_EXPORT const unsigned char podBAVDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podBAVDomain/PublicHeader.h>
+
+
